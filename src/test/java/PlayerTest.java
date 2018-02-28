c8e949f1ad7aacc2735a54dfda2fdbc4d51515e9
Jonathan Cruickshank <ikenna1982@hotmail.com>
2018-02-28 15:35:34 +0000

    adds hand class. methods for counting number of cards in hand and total score, and relevant tests.

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,12 @@
+import org.junit.Before;
+
+public class PlayerTest {
+    Player playerOne;
+    Player playerTwo;
+
+    @Before
+    public void before() {
+        playerOne = new Player();
+        playerTwo = new Player();
+    }
+}
